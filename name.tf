39784921a3144a7cdaed21cb4bf4067ebd46862b
csyed <smachisty92@gmail.com>
2022-03-02 01:48:34 +0530

    terraform

---
+++ name.tf
@@ -0,0 +1,6 @@
+resource "aws_ec2_tag" "tag" {
+  count = length(var.RESOURCE_ID)
+  resource_id = element(var.RESOURCE_ID,count.index)
+  key         = var.TAG_NAME
+  value       = var.TAG_VALUE
+}

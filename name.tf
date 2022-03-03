07b85ea94142042690950fb56fe446e0c7ab55f3
csyed <smachisty92@gmail.com>
2022-03-03 12:27:42 +0530

    terraform

--- name.tf
+++ name.tf
@@ -1,5 +1,5 @@
 resource "aws_ec2_tag" "tag" {
-  count = length(var.RESOURCE_ID)
+  count = length(data.terraform_remote_state.ec2.outputs.ALL_TAGS_IDS)
   resource_id = element(var.RESOURCE_ID,count.index)
   key         = var.TAG_NAME
   value       = var.TAG_VALUE
